00c231185385185068aa460c4f5e81dfefbdebb6
Cordelia Usiokhale <cusiokhale@academic.rrc.ca>
2026-01-11 19:40:54 -0600

    feat: remove the EventCard component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import EventsCard from './components/events-card/EventsCard';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -26,8 +25,6 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-
-      <EventsCard/>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
