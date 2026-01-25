3f3c70b24c4513fe54cfa294d53e405679633ee9
Nkechi Echeta <jecheta@academic.rrc.ca>
2026-01-25 13:08:59 -0600

    feat: add nav component with links to pages (T.2).

---
+++ src/components/layout/Nav.tsx
@@ -0,0 +1,25 @@
+import { NavLink } from "react-router-dom";
+
+function Nav() {
+  return (
+    <nav aria-label="Main navigation">
+      <ul>
+        <li>
+          <NavLink to="/" end>
+            Home
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink to="/events">Events</NavLink>
+        </li>
+
+        <li>
+          <NavLink to="/services">Services</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
