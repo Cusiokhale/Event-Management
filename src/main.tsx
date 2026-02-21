da1c98527dbb5977f5b4054f87b9fa74a76770d9
Cordelia Usiokhale <cusiokhale@academic.rrc.ca>
2026-02-21 14:19:51 -0600

    refactor(T4): wrap app with SharedMessageProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
+import { SharedMessageProvider } from "./contexts/sharedMessageContext";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <SharedMessageProvider>
+        <App />
+      </SharedMessageProvider>
     </BrowserRouter>
   </StrictMode>
 );
